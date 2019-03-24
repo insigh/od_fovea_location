37575d8928192048672e5a40baf0f42e3c328cce
insigh <zhanghchaojie@ruc.edu.cn>
2019-03-24 21:35:50 +0800

    draw generated od bbox

--- data/draw_bbx.py
+++ data/draw_bbx.py
@@ -26,7 +26,7 @@ def draw_anchor(ImgPath, AnnoPath, save_path):
         anns = json.load(annsfile)
         print(len(anns))
         for idx, ann in enumerate(anns):
-            # print(idx, ann)
+            print(idx, ann)
             count += 1
             image_id = ann['image_id']
             image_filename = image_id_to_filenames[str(image_id)]
@@ -43,7 +43,7 @@ def draw_anchor(ImgPath, AnnoPath, save_path):
 
             img = cv.imread(image_path)
             print(img.shape)
-
+            #
             cv.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), thickness=2)
             cv.putText(img, 'od', (x1, y1), cv.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0),
                        thickness=2)
@@ -53,44 +53,5 @@ def draw_anchor(ImgPath, AnnoPath, save_path):
             cv.imwrite(save_path + '/' + image_filename, img)  # save picture
     print(count)
 
-
-
-
-
-
-        # image_pre, ext = os.path.splitext(image)
-        # imgfile = ImgPath + image
-        #
-        #
-        # img = cv.imread(imgfile)
-        #
-        #
-        #
-        #
-        # # 得到标签名为object的信息
-        # objectlist = collection.getElementsByTagName("object")
-        #
-        # for objects in objectlist:
-        #     # 每个object中得到子标签名为name的信息
-        #     namelist = objects.getElementsByTagName('name')
-        #     # 通过此语句得到具体的某个name的值
-        #     objectname = namelist[0].childNodes[0].data
-        #
-        #     bndbox = objects.getElementsByTagName('bndbox')
-        #     # print(bndbox)
-        #     for box in bndbox:
-        #         x1_list = box.getElementsByTagName('xmin')
-        #         x1 = int(x1_list[0].childNodes[0].data)
-        #         y1_list = box.getElementsByTagName('ymin')
-        #         y1 = int(y1_list[0].childNodes[0].data)
-        #         x2_list = box.getElementsByTagName('xmax')  # 注意坐标，看是否需要转换
-        #         x2 = int(x2_list[0].childNodes[0].data)
-        #         y2_list = box.getElementsByTagName('ymax')
-        #         y2 = int(y2_list[0].childNodes[0].data)
-        #         cv.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), thickness=2)
-        #         cv.putText(img, objectname, (x1, y1), cv.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0),
-        #                    thickness=2)
-        #         # cv.imshow('head', img)
-        #         cv.imwrite(save_path + '/' + filename, img)  # save picture
 
 draw_anchor(ImgPath, AnnoPath, save_path)
